669c27b9a48b083f727ca35dd581d06895107006
Your Name <you@example.com>
2019-07-09 15:03:43 -0700

    initial commit

---
+++ aboutme.js
@@ -0,0 +1,11 @@
+alert("Welcome to my page! I hope you like it :)");
+
+function bigImg(x){
+  x.style.height = "400px";
+  x.style.width = "950px";
+}
+
+function originlIMG(x){
+  x.style.height = "300px";
+  x.style.width = "500px";
+}
